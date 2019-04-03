9ff0feabecf891fca45a66df70da92601a211edb
Unknown <up201603634@fe.up.pt>
2019-04-03 13:10:33 +0100

    product_list table created
    Query ao fazer checkout, mudar os ass_product_list para bought e cria um novo cart. done

--- sql/queries.sql
+++ sql/queries.sql
@@ -5,20 +5,24 @@ SELECT password
 FROM person P
 WHERE P.email LIKE $email;
 
+
 --query informaçao pessoal do client 
 SELECT name, email, nif
 FROM person P  JOIN client USING(id)
 WHERE P.email LIKE $email; --P.id = $id;
+
 
 --query moradas do client
 SELECT A.name, address_line, postal_code, country, city
 FROM address A , person P 
 WHERE A.id_client = P.id AND P.email LIKE $email;  --P.id = $id;
 
+
 --query cartoes do client
 SELECT last_digits, expiration_date, CC.name, type
 FROM credit_card CC , person P 
 WHERE CC.id_client = P.id AND P.email LIKE $email; --P.id = $id;
+
 
 --query wish lists do client
 SELECT WL.id, WL.name, WL.description
@@ -27,7 +31,6 @@ WHERE P.id = WL.id_client AND P.email LIKE $email; --P.id = $id;
 
 
 --query carts do client
-
 SELECT C.checkout, TP.price
 FROM cart C, client CL, (
 SELECT AP.id_list ,SUM(AP.TotalPrice) AS price
@@ -36,8 +39,7 @@ select ALP.id_list, ALP.quantity, P.id ,P.price, price*quantity AS TotalPrice
 from ass_list_product ALP, product P
 WHERE ALP.id_product = P.id ) AS AP
 GROUP BY AP.id_list) AS TP
-WHERE C.id_list = TP.id_list AND CL.id = C.id_client AND CL.id = $id;
-
+WHERE C.id = TP.id_list AND CL.id = C.id_client AND CL.id = $id;
 
 
 --query de categorias TODO testing
@@ -59,15 +61,18 @@ WHERE M.id = R.id_message AND M.id_product = P.id)
 FROM product P, category C	
 WHERE P.id_category = C.id AND P.id = $id;
 
+
 --query de specifications de produtos 
 SELECT  SH.name , SB.content, APS.id_product
 FROM specification S, specification_body SB, specification_header SH, ass_product_specification APS, product P
 WHERE S.id_specification_body = SB.id AND S.id_specification_header = SH.id AND APS.id_specification = S.id AND P.id = APS.id_product AND P.id = $id;
 
+
 --query todas imagens de um produto
 SELECT I.filepath, I.description
 FROM image I
 WHERE I.id_product = $id;
+
 
 --query imagem principal de um produto
 SELECT I.filepath, I.description
@@ -80,15 +85,18 @@ SELECT 	ALP.id_product, ALP.quantity, ALP.added_to, ALP.bought, ALP.return
 FROM ass_list_product ALP
 WHERE ALP.id_list = $id;
 
+
 --query informacao dos produtos de uma wishlist TODO testing
 SELECT P.id, P.name, P.price, P.ranking, I.filepath, I.description, ALP.added_to, ALP.quantity, ALP.bought, ALP.returned
 FROM wish_list WL, ass_list_product ALP, product P, image I
 WHERE WL.id = $id AND WL.id = ALP.id_list AND P.id = ALP.id_product AND I.id_product = P.id AND I.primary_img = 'TRUE';
 
+
 --query informacao dos produtos de um cart TODO testing
 SELECT P.id, P.name, P.price, P.ranking, I.filepath, I.description, ALP.added_to, ALP.quantity, ALP.bought, ALP.returned
 FROM cart C, ass_list_product ALP, product P, image I
 WHERE C.id = $id AND C.id = ALP.id_list AND P.id = ALP.id_product AND I.id_product = P.id AND I.primary_img = 'TRUE';
+
 
 --INSERT
 
@@ -109,6 +117,7 @@ INSERT INTO client
 SELECT id, $nif
 FROM ins;
 
+
 --query insert review (creates new message and review)
 WITH ins AS (
 INSERT INTO message
@@ -119,6 +128,7 @@ INSERT INTO review
 (id_message, rating)
 SELECT id, $rating
 FROM ins;
+
 
 --query insert q_a question (creates new message and q_a)
 WITH ins AS (
@@ -131,6 +141,7 @@ INSERT INTO q_a
 SELECT id
 FROM ins;
 
+
 --query insert q_a answer (creates new message and q_a) updates q_a 
 WITH ins AS (
 INSERT INTO message
@@ -141,10 +152,12 @@ UPDATE q_a
 SET id_answer =  (SELECT id FROM ins)
 WHERE id_message = $id;
 
+
 --insert product info when category exists
 INSERT INTO product
   (name, price, stock, id_category)
   VALUES ($name, $price, $stock, (SELECT id FROM category WHERE name LIKE $category)) ;
+
 
 --insert product specification when specification_body exists
 WITH ins AS (
@@ -170,6 +183,26 @@ INSERT INTO ass_list_product
 VALUES ($id_list, $id_product, $quantity, $added_to, 'FALSE', 'FALSE');
 
 
+--insert cart
+WITH ins AS (
+INSERT INTO product_list DEFAULT VALUES
+  RETURNING id)
+INSERT INTO cart
+(id, id_client)
+SELECT id, $id_client
+FROM ins; 
+
+
+--insert wish_list
+WITH ins AS (
+INSERT INTO product_list DEFAULT VALUES
+  RETURNING id)
+INSERT INTO wish_list
+(id, id_client, name, description)
+SELECT id, $id_client, $name, $description
+FROM ins; 
+
+
 --UPDATE
 
 --query update block non-admin
@@ -188,12 +221,21 @@ UPDATE ass_list_product
   WHERE id_list = $id;
 
 
+--query update ass_list_product to bought, checkout date in cart and create new cart for client
 BEGIN TRANSACTION;
   UPDATE ass_list_product
-    SET bought = 'FALSE'
-    WHERE id_list = 1;
+    SET bought = 'TRUE'
+    WHERE id_list = $id_cart;
   UPDATE cart
-    SET checkout = '2018-12-12'
-    WHERE id_list = 1;
+    SET checkout = $checkout
+    WHERE id = $id_cart;
+  WITH ins AS (
+    INSERT INTO product_list DEFAULT VALUES
+    RETURNING id)
+  INSERT INTO cart
+  (id, id_client)
+  SELECT I.id, C.id_client
+  FROM ins I, cart C
+  WHERE C.id = $id_cart;
 COMMIT;
 
