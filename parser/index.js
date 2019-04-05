3087fa06de2fe4718d265f6afc13a9a56b050d83
Gonçalo Regueiras dos Santos <up201603265@fe.up.pt>
2019-04-05 18:12:18 +0100

    updated parser

--- parser/index.js
+++ parser/index.js
@@ -7,10 +7,12 @@ const book_id = "270419";
 const desktop_id = "428651031";
 const mouse_id = "430567031";
 const music_id = "11055011";
+const headphones_id = "4085731";
 
 //fork("./parse_category.js", [keyboard_id, "keyboard"]);
 //fork("./parse_category.js", [laptop_id, "laptop"]);
 //fork("./parse_category.js", [book_id, "book"]);
 //fork("./parse_category.js", [desktop_id, "desktop"]);
 //fork("./parse_category.js", [mouse_id, "mouse"]);
-fork("./parse_category.js", [music_id, "music"]);
+//fork("./parse_category.js", [music_id, "music"]);
+fork("./parse_category.js", [headphones_id, "headphones"]);
