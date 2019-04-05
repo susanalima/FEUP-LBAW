6968993eaea4713909e7da4d13489327daa9d09f
Gonçalo Regueiras dos Santos <up201603265@fe.up.pt>
2019-04-05 18:22:25 +0100

    updated headphone id

--- parser/index.js
+++ parser/index.js
@@ -7,7 +7,7 @@ const book_id = "270419";
 const desktop_id = "428651031";
 const mouse_id = "430567031";
 const music_id = "11055011";
-const headphones_id = "4085731";
+const headphones_id = "4222329031";
 
 //fork("./parse_category.js", [keyboard_id, "keyboard"]);
 //fork("./parse_category.js", [laptop_id, "laptop"]);
