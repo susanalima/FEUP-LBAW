b43238e9c394b2079ea8d88cd5c03769ccb347fc
Afonso <afonsojoseazevedo@gmail.com>
2019-04-07 17:46:56 +0100

    Removed redundant file, progress on trigger

--- sql/product_spec_trigger.sql
+++ sql/product_spec_trigger.sql
@@ -3,12 +3,18 @@ DROP FUNCTION IF EXISTS check_specs() CASCADE;
 CREATE FUNCTION check_specs() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-IF NEW.state
+IF NEW.available = true THEN
 IF EXISTS(
-    WITH specs AS(
+    WITH 
+    specs_associations AS(
+        select id_specification
+        FROM ass_product_specification PS 
+        where PS.id_product = new.id
+    ),
+    specs AS(
         SELECT SP.id, SP.id_specification_header
-        FROM specification SP
-        Where NEW.id_specification = SP.id
+        FROM specs_associations SA, specifications SP
+        Where SA.id_specification = SP.id
     ),
     product_headers AS(
         SELECT SH.id
@@ -19,7 +25,7 @@ IF EXISTS(
     products_category AS(
         SELECT C1.id
         FROM category C1, product PR
-        WHERE NEW.id_product = PR.id and C1.id = PR.id_category
+        WHERE NEW.id = PR.id and C1.id = PR.id_category
     ),
     category_headers AS(
         SELECT IH.id
@@ -30,13 +36,14 @@ IF EXISTS(
     ((SELECT * FROM product_headers EXCEPT SELECT * FROM category_headers) UNION ALL (SELECT * FROM category_headers EXCEPT SELECT * FROM product_headers)))
     THEN RAISE EXCEPTION 'The product added does not have the required specifications for its category.';
     END IF;
+    END IF;
     RETURN NEW;
 END
 $BODY$
 LANGUAGE plpgsql;
 
 
-DROP TRIGGER IF EXISTS product_spec_trigger ON ass_product_specification CASCADE;
-CREATE TRIGGER product_spec_trigger AFTER UPDATE ON ass_product_specification
+DROP TRIGGER IF EXISTS product_spec_trigger ON product CASCADE;
+CREATE TRIGGER product_spec_trigger AFTER UPDATE ON product
 FOR EACH ROW
 EXECUTE PROCEDURE check_specs();
