1a71b6e826dcf95d4bd5d61d2765af053ccf0f4a
Gonçalo Regueiras dos Santos <up201603265@fe.up.pt>
2019-04-09 09:21:01 +0100

    Base Laravel structure

--- parser/q.sql
+++ parser/q.sql
@@ -1,5 +1,3 @@
-SELECT json_object_agg(each.id, each.specs) FROM (
-select C.name AS name, C.id ,array_agg(SH.name) AS specs
-from category C, ass_category_specification ACS, specification_header SH
-where ACS.id_category = C.id AND ACS.id_specification_header = SH.id 
- GROUP BY C.id) AS each;
+select * 
+from product
+where to_tsvector('english', name) @@ plainto_tsquery('english', 'computer') ORDER BY ts_rank(to_tsvector('english', name), plainto_tsquery('english', 'computer')) desc
