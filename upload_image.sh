8119fa93a7fbc342a4c508c16ddd78c8aa1f00a6
Gonçalo Regueiras dos Santos <up201603265@fe.up.pt>
2019-05-05 12:12:54 +0100

    crlf changes

--- upload_image.sh
+++ upload_image.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# Stop execution if a step fails
-set -e
-
-DOCKER_USERNAME=lbaw31   # Replace by your docker hub username
-IMAGE_NAME=lbaw1831-piu            # Replace with your group's image name
-
-# Ensure that dependencies are available
-composer install
-php artisan clear-compiled
-php artisan optimize
-
-docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
-docker push $DOCKER_USERNAME/$IMAGE_NAME
+#!/bin/bash# Stop execution if a step failsset -eDOCKER_USERNAME=lbaw31   # Replace by your docker hub usernameIMAGE_NAME=lbaw1831-piu            # Replace with your group's image name# Ensure that dependencies are availablecomposer installphp artisan clear-compiledphp artisan optimizedocker build -t $DOCKER_USERNAME/$IMAGE_NAME .docker push $DOCKER_USERNAME/$IMAGE_NAME
