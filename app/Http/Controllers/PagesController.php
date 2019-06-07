afab4b6634cee300cbdf953fe0541df21cfad0dd
Gonçalo Regueiras dos Santos <up201603265@fe.up.pt>
2019-06-07 11:57:47 +0100

    bug fixes

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -21,7 +21,7 @@ class PagesController extends Controller
 {
  private function cart()
  {
-  if (Auth::check()) {
+  if (Auth::check() && Client::find(Auth::user()->id) !== null) {
    return Client::find(Auth::user()->id)->cart();
   } else {
    return [];
@@ -48,7 +48,7 @@ class PagesController extends Controller
  {
   $cart = $this->cart();
 
-  if (Auth::check() && $cart->get(0) === null) {
+  if (Auth::check() && $cart && $cart->get(0) === null) {
    PagesController::createCart(Auth::user()->id);
    $cart = $this->cart();
   }
