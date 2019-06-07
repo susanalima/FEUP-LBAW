1766062ead18cc906fbbe40676873b1ae814a7e8
vieirajlt <up201603190@fe.up.pt>
2019-06-07 13:09:34 +0100

    Auto stash before merge of "proto" and "origin/proto"

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -27,6 +27,7 @@ class PagesController extends Controller
             return [];
         }
 
+<<<<<<< Updated upstream
     }
 
     private function createCart($client_id)
@@ -504,6 +505,507 @@ class PagesController extends Controller
     }
 
     public function wishList($id)
+=======
+    }
+
+    private function createCart($client_id)
+    {
+        $request = [];
+
+        $request['client_id'] = $client_id;
+        $request['list_id'] = ProductList::firstOrCreate(['id' => ProductList::max('id') + 1]);
+        Cart::firstOrCreate([
+            'id' => $request['list_id']['id'],
+            'id_client' => $request['client_id'],
+            'checkout' => null,
+            'id_card' => null,
+            'id_address' => null,
+            'id_shipping' => null]);
+    }
+
+    public function makeCart()
+    {
+        $cart = $this->cart();
+
+        if (Auth::check() && $cart && $cart->get(0) === null) {
+            PagesController::createCart(Auth::user()->id);
+            $cart = $this->cart();
+        }
+        $total = 0;
+        $prod_ids = [];
+        if ($cart != []) {
+            $cart['products'] = $cart->get(0)->list_products();
+            $cart['total'] = 0;
+
+            foreach ($cart['products'] as $product) {
+
+                array_push($prod_ids, $product->id);
+
+                $product->name = str_before($product->name, ' -');
+
+                $tmp = DB::select("SELECT quantity, added_to FROM ass_list_product WHERE id_list = {$cart[0]['id']} and id_product = $product->id")[0];
+                $img = DB::select("SELECT filepath, description FROM image WHERE primary_img = true and id_product = $product->id")[0];
+
+                $product->img_path = $img->filepath;
+                $product->img_description = $img->description;
+
+                $total += $product->price * $product->quantity;
+            }
+            $cart['total'] = $total;
+            $cart['prod_ids'] = $prod_ids;
+
+        } else {
+            $cart['products'] = [];
+            $cart['total'] = 0;
+        }
+
+        $cart['prod_ids'] = $prod_ids;
+
+        return $cart;
+    }
+
+    public function index()
+    {
+        $cart = PagesController::makeCart();
+        $promotions = Promotion::active()->get()->random(2)->map(function ($promotion) {
+
+            if (count($promotion->products) > 0) {
+
+                $product = $promotion->products->random(1)[0];
+                $images = $product->images->filter(function ($image) {
+                    return ($image->primary_img);
+
+                });
+
+                $images = $images;
+                if (count($images) > 0) {
+                    return [
+                        'discount' => $promotion->discount,
+                        'name' => $product->name,
+                        'product_id' => $product->id,
+                        'image' => $images->random(1)[0]->filepath,
+                    ];
+                } else {
+                    return null;
+                }
+            }
+
+        });
+
+        $product = Product::where('available', 'true')->orderBy('id', 'desc')->first();
+        $product = [
+            'name' => $product->name,
+            'product_id' => $product->id,
+            'image' => $product->primary_image(),
+        ];
+
+        $wished = array();
+
+        $cats = array(1, 4, 6);
+
+        foreach ($cats as $key => $cat) {
+            $prods = array();
+            $temp = Product::where('id_category', '=', $cat)->withCount('lists')->orderBy('lists_count', 'desc')->take(10)->get();
+            foreach ($temp as $key => $value) {
+                array_push($prods,
+                    [
+                        'name' => str_before($value->name, ' -'),
+                        'price' => $value->price,
+                        'rating' => $value->rating(),
+                        'image' => $value->primary_image(),
+                        'id' => $value->id,
+                    ]);
+            }
+            $wished[Aux::formatHeader(Category::find($cat)->name)] = $prods;
+        }
+
+        $data = array(
+            'interactive' => true,
+            'promos' => $promotions,
+            'product' => $product,
+            'wished' => $wished,
+            'cart' => $cart,
+        );
+
+        return view("index")->with($data);
+    }
+
+    public function help()
+    {
+        $cart = PagesController::makeCart();
+
+        $data = array(
+            'type' => 'help',
+            'interactive' => true,
+            'cart' => $cart,
+        );
+        return view("pages.help")->with($data);
+    }
+
+    public function contacts()
+>>>>>>> Stashed changes
+    {
+
+        $cart = PagesController::makeCart();
+
+<<<<<<< Updated upstream
+        $info = WishList::find($id);
+        $list['id'] = $id;
+        $list['name'] = Aux::formatHeader($info->name);
+        $list['products'] = $info->list_products();
+
+        $this->getProductExtras($list['products']);
+
+        $client_id = $info->id_client;
+        $client = Client::find($client_id);
+
+        $list['all_lists'] = $client->wishLists;
+
+        $data = array(
+            'type' => 'information',
+            'interactive' => true,
+            'info' => $list,
+            'cart' => $cart,
+        );
+
+        return view("pages.wish_list")->with($data);
+    }
+
+=======
+        $data = array(
+            'type' => 'contacts',
+            'interactive' => true,
+            'cart' => $cart,
+        );
+        return view("pages.help")->with($data);
+    }
+
+    public function faq()
+    {
+
+        $cart = PagesController::makeCart();
+
+        $data = array(
+            'type' => 'faq',
+            'interactive' => true,
+            'cart' => $cart,
+        );
+        return view("pages.help")->with($data);
+    }
+
+    public function login()
+    {
+        $data = array(
+            'interactive' => false,
+        );
+        return view("pages.login")->with($data);
+    }
+
+    public function product_create()
+    {
+        $cats = Category::all()->map(function ($category) {
+            return array("id" => $category->id, "name" => $category->name);
+        });
+
+        $specs = Category::all()[0]->specs->map(function ($header) {
+            return array('name' => Aux::formatHeader($header['name']), 'id' => $header['id']);
+        });
+
+        $data = array(
+            'type' => 'add_product',
+            'interactive' => true,
+            'categories' => $cats,
+            'specs' => $specs,
+        );
+
+        $error = Cookie::get('error');
+        if ($error !== null) {
+            $data['error'] = $error;
+        }
+
+        Cookie::queue(Cookie::forget('error'));
+        return view("pages.add_product")->with($data);
+    }
+
+    public function product($id)
+    {
+
+        $cart = PagesController::makeCart();
+
+        $product = Product::find($id);
+
+        $wishlists = array();
+        $collection = array();
+        $product_wl = array();
+        if (Auth::check()) {
+            $client = Client::find(Auth::user()->id);
+            $wishlists = $client->wishLists;
+            $product_wl = $product->wishlists();
+            foreach ($product_wl as $prwl) {
+                array_push($collection, $prwl->id_list);
+            }
+        }
+
+        if ($product === null) {
+            abort('404');
+        }
+
+        $product['category'] = Aux::formatHeader($product->category['name']);
+        $product['images'] = $product->images;
+        $product['specs'] = $product->specifications->map(function ($a) {return $a->spec();});
+        $product['reviews'] = $product->getReviews();
+        $product['q_a'] = $product->getQA();
+        $product['rating'] = $product->rating();
+        $product['wishlists'] = $collection;
+        $product['number_wl'] = count($product_wl);
+
+        $data = array(
+            'type' => 'product',
+            'interactive' => true,
+            'product' => $product,
+            'wishlists' => $wishlists,
+            'cart' => $cart,
+        );
+
+        return view("pages.product")->with($data);
+    }
+
+    public function search($category = null, $text = null, Request $request)
+    {
+        $rules = [
+            'minPrice' => 'nullable|numeric',
+            'maxPrice' => 'nullable|numeric',
+            'order' => 'nullable|alpha|max:5',
+            'stock' => 'nullable|numeric',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+
+            $error = $validator->errors()->first();
+            $cookie = cookie('error', $error);
+
+            return redirect()->route('search')->withCookies([$cookie]);
+        }
+
+        $restrictions = ($request->all());
+
+        $size = 15;
+        $cart = PagesController::makeCart();
+
+        $catAux = Category::find($category);
+        $categoryName = ($catAux != null ? Aux::formatHeader($catAux->name) : 'All Categories');
+
+        $prods = Product::search($text);
+
+        $selectedBrands = [];
+        $order = '';
+        $orderDir = '';
+        if (count($restrictions) !== 0) {
+
+            if (isset($restrictions['order'])) {
+                $order = $restrictions['order'];
+                $orderDir = $restrictions['orderDir'];
+
+                switch ($order) {
+                    case 'price':
+                    case 'name':
+                        $prods = $prods->orderBy($order, $orderDir);
+                        break;
+
+                    case 'date':
+                        $prods = $prods->orderBy('id', $orderDir);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+            if (isset($restrictions['maxPrice']) && isset($restrictions['minPrice'])) {
+                $prods = $prods->where([
+                    ['price', '<=', $restrictions['maxPrice']],
+                    ['price', '>=', $restrictions['minPrice']],
+                ]);
+            }
+
+            if (isset($restrictions['brands'])) {
+                $selectedBrands = $restrictions['brands'];
+            }
+
+            if (count($selectedBrands) !== 0) {
+                $prods = $prods; //->whereIn('brand', $restrictions['brands']);
+            }
+        }
+
+        if ($catAux === null) {
+            $products = $prods->paginate($size);
+        } else {
+            $products = $prods->where('id_category', $category)->paginate($size);
+        }
+
+        $brands = [];
+        $priceRange = [
+            'low' => (isset($restrictions['minPrice']) ? $restrictions['minPrice'] : Product::min('price')),
+            'high' => (isset($restrictions['maxPrice']) ? $restrictions['maxPrice'] : Product::max('price')),
+        ];
+
+        foreach ($products as $product) {
+
+            $product_wl = [];
+            $collection = array();
+            if (Auth::check()) {
+                $client = Client::find(Auth::user()->id);
+                $wishlists = $client->wishLists;
+                $product_wl = $product->wishlists();
+                foreach ($product_wl as $prwl) {
+                    array_push($collection, $prwl->id_list);
+                }
+            }
+
+            $product['wishlists'] = $collection;
+            $product['number_wl'] = count($product_wl);
+
+            $product['rating'] = $product->rating();
+            $specs = $product->specifications->filter(function ($spec) {
+                return ($spec['header']['name'] == 'brand');
+            });
+
+            foreach ($specs as $spec) {
+                $brand = $spec['body']['content'];
+                $brands[Aux::formatHeader($brand)] = 0;
+            }
+
+        }
+
+        $json = json_decode($products->toJson(), true);
+
+        $wishlists = array();
+        if (Auth::check()) {
+            $client = Client::find(Auth::user()->id);
+            $wishlists = $client->wishLists;
+        }
+
+        $data = array(
+            'type' => 'product',
+            'interactive' => true,
+            'products' => $products,
+            'json' => $json,
+            'cart' => $cart,
+            'category' => $categoryName,
+            'categoryNumber' => $category,
+            'searchContent' => $text,
+            'brands' => $brands,
+            'selectedBrands' => $selectedBrands,
+            'price_range' => $priceRange,
+            'order' => $order,
+            'orderDir' => $orderDir,
+            'wishlists' => $wishlists,
+        );
+
+        return view("pages.search")->with($data);
+
+    }
+
+    public function profile()
+    {
+
+        if (!Auth::check()) {
+            abort(404);
+        }
+
+        $ccart = PagesController::makeCart();
+
+        $info = Client::find(Auth::user()->id);
+        $userInfo = User::find(Auth::user()->id);
+        //$info = Client::find($id);
+        $info['id'] = $info->id;
+        $info['name'] = $userInfo->name;
+        $info['email'] = $userInfo->email;
+        $info['nif'] = $info->nif;
+        $info['addresses'] = $info->addresses;
+        $info['cards'] = $info->credit_cards;
+
+        $info['wishLists'] = $info->wishLists;
+        $info['page'] = 'profile';
+
+        $previousCarts = $info->carts->filter(function ($cart) {
+            return $cart->checkout != null;
+        });
+
+        $info['carts'] = $previousCarts->map(function ($cart) {
+
+            $address_line = '';
+            $postal_code = '';
+            $country = '';
+            $city = '';
+            $address_name = '';
+            $card = 'Deleted';
+            $address_deleted = 'true';
+
+            if ($cart->address != null) {
+                $address_line = $cart->address->address_line;
+                $postal_code = $cart->address->postal_code;
+                $country = $cart->address->country;
+                $city = $cart->address->city;
+                $address_name = $cart->address->name;
+                $card = 'Deleted';
+                $address_deleted = 'false';
+            }
+
+            if ($cart->creditCard != null) {
+                $card = $cart->creditCard->last_digits;
+            }
+
+            $products = $cart->list_products();
+
+            $this->getProductExtras($products);
+
+            $tp = 0;
+            foreach ($products as $pt) {
+                $tp = $tp + $pt->price * $pt->quantity;
+            }
+
+            $collection = collect($products);
+
+            return [
+                'id' => $cart->id,
+                'checkout' => $cart->checkout,
+                'address_deleted' => $address_deleted,
+                'address_line' => $address_line,
+                'postal_code' => $postal_code,
+                'country' => $country,
+                'city' => $city,
+                'address_name' => $address_name,
+                'shipping' => $cart->shipping->method,
+                'card' => $card,
+                'products' => $collection,
+                'total' => $tp,
+            ];
+        });
+
+        $data = array(
+            'type' => 'information',
+            'interactive' => true,
+            'info' => $info,
+            'cart' => $ccart,
+        );
+
+        return view("pages.profile")->with($data);
+    }
+
+    public function getProductExtras($products)
+    {
+        foreach ($products as $product) {
+            $c_product = Product::find($product->id);
+            $img_src = $c_product->images[0]['filepath'];
+            $product->image = $img_src;
+            $product->price = $c_product->price;
+            //$product->name = Aux::formatHeader($product->name);
+        }
+    }
+
+    public function wishList($id)
     {
 
         $cart = PagesController::makeCart();
@@ -530,6 +1032,7 @@ class PagesController extends Controller
         return view("pages.wish_list")->with($data);
     }
 
+>>>>>>> Stashed changes
     public function profile_manager($id)
     {
         //TODO
