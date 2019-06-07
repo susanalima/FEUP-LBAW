53af075943f94f2a9a9bff74bf4ab9db99cd314c
susanalima <up201603634@fe.up.pt>
2019-06-07 15:13:04 +0100

    wl modal bug fixed

--- public/js/wishList.js
+++ public/js/wishList.js
@@ -32,17 +32,30 @@ function removeFromWishList(removeButton, product_id, list_id){
 }
 
 
-function addProductToWishList(product_id) {
+function addProductToWishList(product_id, button = null) {
   let opts = document.getElementById(`selectWL${product_id}`);
+  if(opts === null) {
+    if(button != null)
+      button.disabled = true;
+    return;
+  }
   let list_id = opts.options[opts.selectedIndex].value;
 
  //console.log(list_id);
+
+
 
   let btn = document.getElementById(`addToWishList${product_id}`);
   btn.classList.toggle("active");
 
   let sl = document.getElementById(`optionP${product_id}WL${list_id}`);
   sl.remove();
+
+  let options_ = document.getElementsByClassName(`option${product_id}`);
+  if(options_.length === 0) {
+    let optDiv = document.getElementById(`optDiv${product_id}`);
+    optDiv.innerHTML = `<p class="m-3">This Product is already in all your wishlists</p>`;
+  }
 
   sendAjaxRequest('POST', '/api/add_product_wl', {product_id: product_id , list_id: list_id}, nothing);
 }
